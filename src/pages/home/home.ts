d1214f5dbf307b4ae8e27ae7109c872fba3898c0
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-04-03 09:50:41 -0400

    added from https://alligator.io/angular/testing-httpclient/

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
